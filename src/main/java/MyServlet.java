976040f3f24bb9a50099f7a588b711fbeb94b6f2
Keyuan1248 <kz3817@ic.ac.uk>
2021-12-05 17:20:15 +0000

    Commit

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -15,9 +16,13 @@ public class MyServlet extends HttpServlet {
 
 
     }
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+reqBody);
     }
+
 
 
 }
---
+++ src/main/java/Patient.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+
+public class Patient implements Serializable {
+    String name;
+    int id;
+    int phoneNum;
+
+
+
+}
